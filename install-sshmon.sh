02dd71b51614576eebcc0653c3ceff3d2188e6d2
DergMcDergerson <p27182@gmail.com>
2023-03-10 11:21:55 -0500

    added wget env file

--- install-sshmon.sh
+++ install-sshmon.sh
@@ -6,7 +6,7 @@ cd sshmon
 pip install discord-webhook
 wget https://raw.githubusercontent.com/p27182/monitoring/main/sshmon.py
 wget https://raw.githubusercontent.com/p27182/monitoring/main/sshmon.service
-
+wget https://raw.githubusercontent.com/p27182/monitoring/main/.env
 sudo mv sshmon.service /lib/systemd/system/sshmon.service
 sudo systemctl daemon-reload
 sudo systemctl enable sshmon
